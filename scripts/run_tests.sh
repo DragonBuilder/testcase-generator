c3cf95854e60fef840fd0cb5dd02f0fe87b84204
Aneesh Anil <aneesh@geektrust.in>
2023-08-24 20:13:50 +0530

    MTE

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 export $(xargs < ./scripts/env/devtest.env)
+
+## run all tests
 go test -v ./...
+
+## specific functions within a package
+# go test -v testcase-generator/internal/openai -run TestStreamingChat 
+# go test -v testcase-generator/internal -run TestStreamingGenerateTestcases 
